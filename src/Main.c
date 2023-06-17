0bafbef5a41c5218b61924765ee4d5f17b359a00
luccie-cmd <hoedje201@gmail.com>
2023-06-17 15:29:14 +0200

    0003
    added command line argument

--- src/Main.c
+++ src/Main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Lexer.h"
+#include <assert.h>
 
-int main(){
-    lexInit("./main.porg");
+// used from https://github.com/tsoding/bm/blob/master/common/path.c line: 119
+char* shift(int* argc, char*** argv){
+    assert(*argc > 0);
+    char *result = **argv;
+    *argv += 1;
+    *argc -= 1;
+    return result;
+}
+
+int main(int argc, char** argv){
+    const char* const program = argv[0];
+    if(argc < 2){
+        printf("Not enough parameters for `%s`\n", program);
+        exit(1);
+    }
+    // skip the program
+    shift(&argc, &argv);
+
+    // get the first flag aka program
+    char* file = shift(&argc, &argv);
+    lexInit(file);
     return 0;
 }
